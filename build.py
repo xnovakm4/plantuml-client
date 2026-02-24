7eced4cb60d827d380f9593b01ed85e5cf32375f
xnovakm4 <michal.novak@centrum.cz>
2026-02-25 00:00:33 +0100

    fix: use pyinstaller --onefile to fix Windows builds and macOS packaging

--- build.py
+++ build.py
@@ -3,13 +3,15 @@ import PyInstaller.__main__
 import os
 
 if __name__ == '__main__':
+    sep = os.pathsep
     PyInstaller.__main__.run([
         'main.py',
         '--name=PlantUML-Client',
         '--windowed',  # Prevent console window from appearing
+        '--onefile',   # Package into a single executable file
         '--noconfirm', # Overwrite existing output directory
         '--clean',     # Clean PyInstaller cache
-        '--add-data=core:core', # Include the core module/translations
-        '--add-data=ui:ui',     # Include UI modules
+        f'--add-data=core{sep}core', # Include the core module/translations
+        f'--add-data=ui{sep}ui',     # Include UI modules
         # Note: plantuml.jar is intentionally NOT bundled since the app auto-downloads it into its current working directory (or app support dir)
     ])
